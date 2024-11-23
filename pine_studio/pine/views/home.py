765f60451a850324373d2a54d9ee1a062906be86
Rafael <rafael.pinheiro@dmslog.com>
2024-11-23 17:15:47 -0300

    feat: init front

---
+++ pine_studio/pine/views/home.py
@@ -0,0 +1,14 @@
+from django.views.generic import ListView
+from ..models import Creative
+
+
+class HomeView(ListView):
+    template_name = 'front/home.html'
+    context_object_name = 'creatives'
+
+    def get_queryset(self):
+        return Creative.objects.filter(is_free=False)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)   
+        return context

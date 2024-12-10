6ecd2f3777c85cd07dd414c5f3fa683ab1520366
Rafael <rafael.pinheiro@dmslog.com>
2024-12-10 12:24:24 -0300

    feat: create about layout

--- pine_studio/pine/urls.py
+++ pine_studio/pine/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from .views.home import HomeView
 from .views.pack import PackListView, PackDetailView
+from .views.about import AboutPageView
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
     path('packs/', PackListView.as_view(), name='packs'),
+    path('quem_sou_eu/', AboutPageView.as_view(), name='about'),
     path('<slug:slug>/', PackDetailView.as_view(), name='pack_detail'),
 ]

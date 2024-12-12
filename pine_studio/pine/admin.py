765d7b94f8385d2563a8c7f6e713082422f8e378
Rafael <rafael.pinheiro@dmslog.com>
2024-12-12 17:13:42 -0300

    feat: finish contact layout

--- pine_studio/pine/admin.py
+++ pine_studio/pine/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import creative, pack, download_email
+from .models import creative, pack, download_email, contact
 
 admin.site.register(creative.Creative)
+admin.site.register(contact.ContactMessage)
 admin.site.register(pack.Pack)
 admin.site.register(pack.PackItem)
 admin.site.register(download_email.DownloadEmail)
--- pine_studio/pine/urls.py
+++ pine_studio/pine/urls.py
@@ -2,10 +2,12 @@ from django.urls import path
 from .views.home import HomeView
 from .views.pack import PackListView, PackDetailView
 from .views.about import AboutPageView
+from .views.contact import ContactView
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
     path('packs/', PackListView.as_view(), name='packs'),
     path('quem_sou_eu/', AboutPageView.as_view(), name='about'),
+    path('contato/', ContactView.as_view(), name='contact'),
     path('<slug:slug>/', PackDetailView.as_view(), name='pack_detail'),
 ]
